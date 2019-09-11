7aadaa2ef36750c375e5fb4f34dab830d6168bde
why <you@example.com>
2019-09-11 18:15:19 +0800

    添加

--- src/component/note/note.js
+++ src/component/note/note.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import './note.scss'
 export default class Note extends React.Component {
   render () {
     return (
       <div>
-        Note
+        <div className="note-list">
+          <div className="note-list-item">
+            Note
+          </div>
+        </div>
       </div>
     )
   }
